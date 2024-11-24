254d962f6fc456d95bd95920357f02f21485a221
CoderRzps <ayushpritysingh098@gmail.com>
2024-11-24 14:07:28 +0530

    Update join_requests.py

--- plugins/join_requests.py
+++ plugins/join_requests.py
@@ -1,18 +1,22 @@
-from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from pyrogram import Client, filters
-from pyrogram.types import *
-from motor.motor_asyncio import AsyncIOMotorClient  
-from os import environ as env
-import asyncio, datetime, time
-
+from pyrogram import Client
+from pyrogram.types import ChatJoinRequest
+from bot import Bot  # Ensure Bot is imported from your bot's main file
 
 ACCEPTED_TEXT = "Hi {user}, welcome to {chat}! You can now access the files here."
 
 @Bot.on_chat_join_request()
-async def req_accept(c, m):
+async def req_accept(c: Client, m: ChatJoinRequest):
     user_id = m.from_user.id
     chat_id = m.chat.id
-    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
+
+    # Approve the join request
     await c.approve_chat_join_request(chat_id, user_id)
-    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
-    except Exception as e: print(e)
+
+    # Send a welcome message
+    try:
+        await c.send_message(
+            user_id,
+            ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title)
+        )
+    except Exception as e:
+        print(f"Error sending message to {user_id}: {e}")
